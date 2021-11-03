71168d806ceedabf21620c55409ebe7e1ee4534b
Ava Wulkan <ava.wulkan@gmail.com>
2021-11-03 11:00:26 +0100

    Revert "Made loops for questions and saved answers"
    This reverts commit 5b65b42d51e724239a05df79d40fab55c6d5a67b.

--- Frontend/quizzingfrontend/src/components/Quiz.vue
+++ Frontend/quizzingfrontend/src/components/Quiz.vue
@@ -3,30 +3,24 @@
   <div>
 <!--        <div v-for="quiz in quizes" v-bind:key="quiz.quizId">{{quiz}}</div>-->
 
-      <input type="text" v-model="quizIndex">
-      <button type="submit" v-on:click="idFirstQuestion(); numberOfQuestions(); questionsInQuiz();">Start quiz</button>
+      <input type="text" v-model="test">
+      <button type="submit" v-on:click="idFirstQuestion(); numberOfQuestions(); questionsInQuiz();"></button>
 
 
 
     <input type="text" v-on:keydown="numberOfQuestions">
 
-    <div>{{quizes.quiz[0].nameQuiz}}</div>
+    <div>{{id_First_Question}} {{number_Of_Questions}} {{quizes.quiz[0].nameQuiz}}</div>
 
     <div v-for="question in questions" v-bind:key="question.questionsId">
-      <h2>{{question[0].question}}</h2>
+      {{question[0].question}}
       <ul>
-        <li><button @click="clickAnswer(question[0].a1, question[0].correctAnswer);">{{question[0].a1}}</button></li>
-        <li><button @click="clickAnswer(question[0].a2, question[0].correctAnswer)">{{question[0].a2}}</button></li>
-        <li><button @click="clickAnswer(question[0].a3, question[0].correctAnswer)">{{question[0].a3}}</button></li>
-        <li><button @click="clickAnswer(question[0].correctAnswer, question[0].correctAnswer)">{{question[0].correctAnswer}}</button></li>
+        <li>{{question[0].a1}}</li>
+        <li>{{question[0].a2}}</li>
+        <li>{{question[0].a3}}</li>
+        <li>{{question[0].correctAnswer}}</li>
       </ul>
     </div>
-    <div>
-      <p v-if="clickedAnswerMessage.length > 1">{{clickedAnswerMessage}}</p>
-    </div>
-      <button v-if="questionAnswered" type ="submit" @click="nextQuestion()">Next question</button>
-      <p>{{studentAnswers}}</p>
-      <p>{{correctAnswers}}</p>
   </div>
 
 
@@ -40,17 +34,9 @@ export default {
       questions: [],
       number_Of_Questions: 0,
       id_First_Question: 0,
-      quizIndex: 0,
+      test: 0,
       quizId: 0,
-      questionId: 0,
-      questionAnswered: false,
-      correctBool: false,
-      clickedAnswerMessage: "",
-      lastAnswer: "",
-      studentAnswers: [],
-      correctAnswer: "",
-      correctAnswers: [],
-      questionIndex: 1
+      questionId: 0
     }
   },
   mounted() {
@@ -60,48 +46,20 @@ export default {
   },
   methods: {
     numberOfQuestions() {
-      fetch('http://localhost:3000/api/quiz/numberofquestions/' + this.quizIndex)
+      fetch('http://localhost:3000/api/quiz/numberofquestions/' + this.test)
           .then(res => res.json())
           .then(data => this.number_Of_Questions = data)
     },
     idFirstQuestion() {
-      fetch('http://localhost:3000/api/quiz/id_firstquestion/' + this.quizIndex)
+      fetch('http://localhost:3000/api/quiz/id_firstquestion/' + this.test)
           .then(res => res.json())
           .then(data => this.id_First_Question = data)
     },
     questionsInQuiz() {
-      fetch('http://localhost:3000/api/quiz' + this.quizIndex + '/question' + this.questionIndex)
+      fetch('http://localhost:3000/api/quiz' + this.test + '/question' + this.test)
           .then(res => res.json())
           .then(data => this.questions = data)
-    },
-    
-     nextQuestion() {
-       this.questionAnswered = false
-       this.questionIndex++
-
-      fetch('http://localhost:3000/api/quiz' + this.quizIndex + '/question' + this.questionIndex)
-          .then(res => res.json())
-          .then(data => this.questions = data)
-          this.clickedAnswerMessage = ""
-          this.studentAnswers.push(this.lastAnswer)
-          this.correctAnswers.push(this.correctAnswer)
-
-    },
-
-    clickAnswer(selected, correct) {
-        this.questionAnswered = true
-        if(selected === correct){
-          this.correctBool = true
-          this.clickedAnswerMessage = "You got basic math right, you rock!"
-        } else {
-        this.clickedAnswerMessage = "Fucking idiot, you got basic math wrong!"
-        }
-
-        this.lastAnswer = selected
-        this.correctAnswer = correct
-    },
-
-
+    }
 
   }
 }
