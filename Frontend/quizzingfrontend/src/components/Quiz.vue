580dbd941aea4c2451484a6cb7ebf3000177a9ac
Johnny Olsen <johnny.olsen@iths.se>
2021-11-04 11:48:35 +0100

    Added timeouts so that methods can call other methods

--- Frontend/quizzingfrontend/src/components/Quiz.vue
+++ Frontend/quizzingfrontend/src/components/Quiz.vue
@@ -1,10 +1,9 @@
 <template>
 
   <div>
-<!--        <div v-for="quiz in quizes" v-bind:key="quiz.quizId">{{quiz}}</div>-->
 
       <input type="text" v-model="quizIndex">
-      <button type="submit" v-on:mousedown="idFirstQuestion(); numberOfQuestions(); questionsInQuiz();" v-on:mouseup="randomMethod(number_Of_Questions); shuffleList()">Start quiz</button>
+      <button type="submit" v-on:mousedown="idFirstQuestion(); numberOfQuestions(); questionsInQuiz();">Start quiz</button>
 
     <div>{{quizes.quiz[0].nameQuiz}}</div>
 
@@ -13,17 +12,10 @@
       <button type="submit" v-on:click="getAnswers(question[0].a1, question[0].a2, question[0].a3, question[0].correctAnswer)">Get Answers</button>
       <ul>
 
-        
-
        <li v-for="element in answerList" :key="element">
           <button @click="clickAnswer(element, answerList.correctAnswer)">
             {{element}}
             </button></li>
-
-
-
-
-
 
       <!--  <li><button @click="clickAnswer(question[0].a1, question[0].correctAnswer);">{{question[0].a1}}</button></li>
         <li><button @click="clickAnswer(question[0].a2, question[0].correctAnswer)">{{question[0].a2}}</button></li>
@@ -36,12 +28,11 @@
       <p v-if="clickedAnswerMessage.length > 1">{{clickedAnswerMessage}}</p>
        <button v-if="questionAnswered && questionIndex < this.number_Of_Questions" type ="submit" @click="nextQuestion();">Next question</button>
        <button v-if="questionAnswered && questionIndex == this.number_Of_Questions" type ="submit" @click=" nextQuestion(); finishQuiz();">Finish quiz</button>
-       <p>{{number_Of_Questions}}</p>
+       <p>Number of questions: {{number_Of_Questions}}</p>
        <p>{{totalPoints}}</p>
        <p>{{answerList}}</p>
     </div>
      
-
       <p>{{studentAnswers}}</p>
       <p>{{correctAnswers}}</p>
     
@@ -83,6 +74,10 @@ export default {
       fetch('http://localhost:3000/api/quiz/numberofquestions/' + this.quizIndex)
           .then(res => res.json())
           .then(data => this.number_Of_Questions = data)
+            setTimeout(() => {
+              this.parseNumberOfQuestionsToInt(this.number_Of_Questions);  // this is fine
+            }, 200)
+          
     },
     idFirstQuestion() {
       fetch('http://localhost:3000/api/quiz/id_firstquestion/' + this.quizIndex)
@@ -93,6 +88,9 @@ export default {
       fetch('http://localhost:3000/api/quiz' + this.quizIndex + '/question' + this.questionIndex)
           .then(res => res.json())
           .then(data => this.questions = data)
+          setTimeout(() => {
+            this.success();  // this is fine
+              }, 200)
     },
     
      nextQuestion() {
@@ -130,7 +128,7 @@ export default {
         
       }
     },
-    randomMethod(x){
+    parseNumberOfQuestionsToInt(x){
       var parsedobj = JSON.parse(JSON.stringify(x))
       parsedobj = Object.values(parsedobj)
       this.number_Of_Questions = parsedobj[0]
@@ -138,12 +136,13 @@ export default {
     getAnswers(a1, a2, a3, correctAnswer){
        const tempList = [a1, a2, a3, correctAnswer];
        this.answerList = tempList
-      console.log(tempList)
+       console.log(tempList)
+    },
+
+    success() {
+      console.log("HAII")
     }
     
-
-
-
   },
 }
 
