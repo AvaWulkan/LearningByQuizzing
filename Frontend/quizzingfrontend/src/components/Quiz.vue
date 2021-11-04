dd09b67ce6f3cdf84e537b4c8399ae0dc24831ea
NovaAnte <andreas_cabraja@hotmail.com>
2021-11-04 12:19:27 +0100

    Fixed randomizing

--- Frontend/quizzingfrontend/src/components/Quiz.vue
+++ Frontend/quizzingfrontend/src/components/Quiz.vue
@@ -1,26 +1,21 @@
 <template>
 
   <div>
+<!--        <div v-for="quiz in quizes" v-bind:key="quiz.quizId">{{quiz}}</div>-->
 
       <input type="text" v-model="quizIndex">
-      <button type="submit" v-on:mousedown="idFirstQuestion(); numberOfQuestions(); questionsInQuiz();">Start quiz</button>
+      <button type="submit" v-on:mousedown="idFirstQuestion(); numberOfQuestions(); questionsInQuiz();" v-on:mouseup="randomMethod(number_Of_Questions); shuffleList()">Start quiz</button>
 
     <div>{{quizes.quiz[0].nameQuiz}}</div>
 
     <div v-for="question in questions" v-bind:key="question.questionsId">
       <h2>{{question[0].question}}</h2>
-      <button type="submit" v-on:click="getAnswers(question[0].a1, question[0].a2, question[0].a3, question[0].correctAnswer)">Get Answers</button>
       <ul>
 
-       <li v-for="element in answerList" :key="element">
-          <button @click="clickAnswer(element, answerList.correctAnswer)">
-            {{element}}
-            </button></li>
-
-      <!--  <li><button @click="clickAnswer(question[0].a1, question[0].correctAnswer);">{{question[0].a1}}</button></li>
+        <li><button @click="clickAnswer(question[0].a1, question[0].correctAnswer);">{{question[0].a1}}</button></li>
         <li><button @click="clickAnswer(question[0].a2, question[0].correctAnswer)">{{question[0].a2}}</button></li>
         <li><button @click="clickAnswer(question[0].a3, question[0].correctAnswer)">{{question[0].a3}}</button></li>
-        <li><button @click="clickAnswer(question[0].correctAnswer, question[0].correctAnswer)">{{question[0].correctAnswer}}</button></li>-->
+        <li><button @click="clickAnswer(question[0].correctAnswer, question[0].correctAnswer)">{{question[0].correctAnswer}}</button></li>
       </ul> 
 
     </div>
@@ -28,11 +23,12 @@
       <p v-if="clickedAnswerMessage.length > 1">{{clickedAnswerMessage}}</p>
        <button v-if="questionAnswered && questionIndex < this.number_Of_Questions" type ="submit" @click="nextQuestion();">Next question</button>
        <button v-if="questionAnswered && questionIndex == this.number_Of_Questions" type ="submit" @click=" nextQuestion(); finishQuiz();">Finish quiz</button>
-       <p>Number of questions: {{number_Of_Questions}}</p>
+       <p>{{number_Of_Questions}}</p>
        <p>{{totalPoints}}</p>
        <p>{{answerList}}</p>
     </div>
      
+
       <p>{{studentAnswers}}</p>
       <p>{{correctAnswers}}</p>
     
@@ -74,10 +70,6 @@ export default {
       fetch('http://localhost:3000/api/quiz/numberofquestions/' + this.quizIndex)
           .then(res => res.json())
           .then(data => this.number_Of_Questions = data)
-            setTimeout(() => {
-              this.parseNumberOfQuestionsToInt(this.number_Of_Questions);  // this is fine
-            }, 200)
-          
     },
     idFirstQuestion() {
       fetch('http://localhost:3000/api/quiz/id_firstquestion/' + this.quizIndex)
@@ -88,13 +80,9 @@ export default {
       fetch('http://localhost:3000/api/quiz' + this.quizIndex + '/question' + this.questionIndex)
           .then(res => res.json())
           .then(data => this.questions = data)
-          setTimeout(() => {
-            this.success();  // this is fine
-              }, 200)
     },
     
      nextQuestion() {
-      // randomizeAnswerList() 
        this.questionAnswered = false
        this.questionIndex++
 
@@ -104,6 +92,8 @@ export default {
           this.clickedAnswerMessage = ""
           this.studentAnswers.push(this.lastAnswer)
           this.correctAnswers.push(this.correctAnswer)
+
+          this.randomize()
 
     },
 
@@ -128,7 +118,7 @@ export default {
         
       }
     },
-    parseNumberOfQuestionsToInt(x){
+    randomMethod(x){
       var parsedobj = JSON.parse(JSON.stringify(x))
       parsedobj = Object.values(parsedobj)
       this.number_Of_Questions = parsedobj[0]
@@ -136,13 +126,19 @@ export default {
     getAnswers(a1, a2, a3, correctAnswer){
        const tempList = [a1, a2, a3, correctAnswer];
        this.answerList = tempList
-       console.log(tempList)
+      console.log(tempList)
     },
+    randomize(){
+      var ul = document.querySelector('ul');
+for (var i = ul.children.length; i >= 0; i--) {
+    ul.appendChild(ul.children[Math.random() * i | 0]);
+}
 
-    success() {
-      console.log("HAII")
     }
-    
+
+
+
+
   },
 }
 
