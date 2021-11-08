3fc40bd9579e559733f2b2b4353e6b8416155a1d
Ava Wulkan <ava.wulkan@gmail.com>
2021-11-08 10:27:26 +0100

    Skrivit ut resultatet

--- Frontend/quizzingfrontend/src/components/QuizStatistics.vue
+++ Frontend/quizzingfrontend/src/components/QuizStatistics.vue
@@ -1,7 +1,14 @@
 <template>
 <div>
-  <h1>Du fick {{ $parent.totalPoints}} poäng</h1>
-
+  <h1>Du fick {{ $parent.totalPoints}} / {{$parent.correctAnswers.length}} poäng</h1>
+  <div v-for="answer in $parent.studentAnswers" v-bind:key="answer">
+    <p>Ditt svar: {{answer}}</p>
+  </div>
+  <div v-for="answer in $parent.correctAnswers" v-bind:key="answer">
+    <p>Rätt svar: {{answer}}</p>
+  </div>
+  <p>{{$parent.studentAnswers}}</p>
+  <p>{{$parent.correctAnswers}}</p>
 </div>
 </template>
 
@@ -9,6 +16,7 @@
 export default {
 
 }
+
 </script>
 
 <style>
