3dc95bcbe4b30e5a228243a665c093a0e51c37ee
Ola Lindgård <ola.lindgard@iths.se>
2021-11-08 13:44:22 +0100

    Lagt grund för css...

--- Frontend/quizzingfrontend/src/components/QuizStatistics.vue
+++ Frontend/quizzingfrontend/src/components/QuizStatistics.vue
@@ -1,12 +1,19 @@
 <template>
-<div>
+<div class="stats-wrapper">
+  <h1>Resultat</h1>
+<div class="points">
 <h1>Du fick {{ $parent.totalPoints}} / {{$parent.correctAnswers.length}} poäng</h1>
 <h1>({{($parent.totalPoints/$parent.correctAnswers.length*100).toFixed(0)}}% Rätt)</h1>
-  <div v-for="answer in $parent.studentAnswers" v-bind:key="answer">
+</div>
+  <div class="stats-container">
+    <!-- göra om divtagen -->
+    </div>
+  <div class="student-answers" v-for="answer in $parent.studentAnswers" v-bind:key="answer">
     <p>Ditt svar: {{answer}}</p>
   </div>
-  <div v-for="answer in $parent.correctAnswers" v-bind:key="answer">
+  <div class="correct-answers" v-for="answer in $parent.correctAnswers" v-bind:key="answer">
     <p>Rätt svar: {{answer}}</p>
+  </div>
   </div>
 <!--  <p>{{$parent.studentAnswers}}</p>
   <p>{{$parent.correctAnswers}}</p>-->
@@ -16,10 +23,66 @@
 <script>
 export default {
 
+  data() {
+
+    return {
+
+      index: 0
+    }
+  },
+
+  mounted() {
+
+
+  }
+
 }
 
 </script>
 
 <style scoped>
+
+.stats-wrapper {
+
+  display: flex;
+  /*justify-content: center; */
+  align-items: center;
+  flex-direction: column;
+
+}
+.points {
+  border: 2px solid black;
+  border-radius: 16px;
+  padding: 20px;
+
+}
+
+.stats-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  border: 2px solid black;
+  border-radius: 16px;
+  padding: 20px;
+  margin-top: 20px;
+}
+
+.questions {
+  border: 2px solid black;
+  border-radius: 16px;
+  padding: 20px;
+}
+
+.student-answers {
+  border: 2px solid black;
+  border-radius: 16px;
+  padding: 20px;
+  margin: 0 20px 0 20px;
+}
+
+.correct-answers {
+  border: 2px solid black;
+  border-radius: 16px;
+  padding: 20px;
+}
 
 </style>
