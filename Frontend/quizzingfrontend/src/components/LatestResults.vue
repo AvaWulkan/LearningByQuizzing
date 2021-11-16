bba371de6de5a144973a1463856c0394aa7ca26e
Johnny Olsen <johnny.olsen@iths.se>
2021-11-16 11:31:17 +0100

    Lagt till så att man kan se tre senaste quizresultaten

--- Frontend/quizzingfrontend/src/components/LatestResults.vue
+++ Frontend/quizzingfrontend/src/components/LatestResults.vue
@@ -1,19 +1,34 @@
 <template>
-<h1>HEJHEJHEJ</h1>
+<div>
+    <div v-for="result in quizResults" :key="result">
+        <h1>{{result.quiz}} {{result.correctAnswers}} {{result.numberOfQuestions}}</h1>
+    </div>
+</div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     data() {
         return {
-
+            quizResults: []
         }
     },
     mounted() {
-        fetch('http://localhost:3000/api/')
+        axios.get('http://localhost:3000/api/quizresults/'+this.$store.state.activeUser)
+        .then(response => {
+            this.quizResults = response.data
+            this.showMostRecentResults()
+        })
     },
     methods: {
-
+        showMostRecentResults() {
+            let element = []
+            for (let i = this.quizResults.question.length-1; i > this.quizResults.question.length-4; i--) {
+                element.push(this.quizResults.question[i]);
+            }
+            this.quizResults = element
+        }
     }
 }
 </script>
