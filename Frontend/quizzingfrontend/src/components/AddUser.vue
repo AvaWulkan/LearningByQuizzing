e581b0adc8cf33126b8f5f980e1dd22176269fb1
NovaAnte <andreas_cabraja@hotmail.com>
2021-11-16 14:29:33 +0100

    Added ability to add teachers/students, added statistics to student homepage

---
+++ Frontend/quizzingfrontend/src/components/AddUser.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <input type="text" v-model="newName" placeholder="Namn">
+      <input type="text" v-model="newUsername" placeholder="Användarnamn">
+      <input type="text" v-model="newPassword" placeholder="Lösenord">
+      <button @click="addNewUser">Skapa användare</button>
+      <p>{{newName}}{{newUsername}}{{newPassword}}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data (){
+        return {
+            newName: "",
+            newUsername: "",
+            newPassword: "",
+            newUserRole: ""
+        }
+    },
+    methods:{
+        addNewUser(){
+            if (this.$store.state.loggedInTeacher){
+                this.newUserRole = 'Student'
+            } else if (this.$store.state.loggedInAdmin){
+                this.newUserRole = 'Teacher'
+            }
+            let stringurl = 'http://localhost:3000/api/createuser/' + this.newName +'/'+ this.newUsername+'/'+ this.newPassword+'/'+this.newUserRole
+            axios.post(stringurl)
+
+            this.newName = "",
+            this.newUsername = "",
+            this.newPassword = "",
+            this.newUserRole = ""
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- Frontend/quizzingfrontend/src/views/Home.vue
+++ Frontend/quizzingfrontend/src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-<p>bajs</p>
-<p>{{$store.state.loggedIn}}</p>
-<p>{{$store.state.loggedInAdmin}}</p>
-<button @click="setLoggedInStudent">Student</button>
-<button @click="setLoggedInTeacher">Teacher</button>
-<button @click="setLoggedInAdmin">Admin</button>
-<p>DET HÄR FUNKAR {{$store.state.activeUser}}</p>
-
+<div v-if="$store.state.loggedInAdmin">
+  <h2>Lägg till lärare</h2>
+  <AddUser/>
+</div>
+<div v-if="$store.state.loggedInTeacher">
+  <h2>Lägg till elev</h2>
+  <AddUser/>
+</div>
+<div v-if="$store.state.loggedInStudent">
+  <h2>Välkommen skitunge!</h2>
+  <StudentStatistics/>
+</div>
   </div>
 </template>
 
 <script>
+import AddUser from '../components/AddUser.vue'
+import StudentStatistics from '../components/StudentStatistics.vue'
+
 export default{
+  components:{
+    AddUser,
+    StudentStatistics
+  },
   methods: {
   setLoggedInStudent(){
     this.$store.commit('setLoggedInStudent')
@@ -22,7 +33,17 @@ export default{
   },
   setLoggedInAdmin(){
     this.$store.commit('setLoggedInAdmin')
+  }
 }
 }
-}
+
+/* <p>bajs</p>
+<p>{{$store.state.loggedIn}}</p>
+<p>{{$store.state.loggedInAdmin}}</p>
+<button @click="setLoggedInStudent">Student</button>
+<button @click="setLoggedInTeacher">Teacher</button>
+<button @click="setLoggedInAdmin">Admin</button>
+<p>DET HÄR FUNKAR {{$store.state.activeUser}}</p>
+*/ 
 </script>
+
